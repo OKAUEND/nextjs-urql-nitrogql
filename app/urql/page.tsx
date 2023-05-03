5aa1e19e93d8aef367cb83d80198e1c7a43278c0
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-03 22:48:26 +0900

    変更 - 子コンポーネントでurqlを使い初期フェッチできるかの確認

---
+++ app/urql/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Client, fetchExchange, gql } from "urql"
+
+const client = new Client({
+  url: "https://beta.pokeapi.co/graphql/v1beta",
+  exchanges: [fetchExchange],
+})
+
+const getPokemons = gql(`
+  query getPokemons {
+    pokemon_v2_pokemon(limit:30, where:{pokemon_species_id: {_gt: 500}}) {
+      name
+      id
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+    }
+  }
+`)
+
+const useUrql = async () => {
+  const result = await client.query(getPokemons, {}).toPromise()
+  console.log({ result })
+  return result
+}
+
+export const UrqlPage = async () => {
+  const result = useUrql()
+
+  return <>TEST</>
+}
