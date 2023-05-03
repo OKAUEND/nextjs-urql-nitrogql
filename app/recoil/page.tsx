ba3a5d45ec596a74a18b5e45268d53325be0b849
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-03 23:08:48 +0900

    変更 - selector内で初期Fetchできた ただ、caught (in promise) Promise {<pending>} のエラーが出ている

--- app/recoil/page.tsx
+++ app/recoil/page.tsx
@@ -1,5 +1,40 @@
 "use client"
 
+import { Client, fetchExchange, gql } from "urql"
+import { selector, useRecoilValue } from "recoil"
+
+const client = new Client({
+  url: "https://beta.pokeapi.co/graphql/v1beta",
+  exchanges: [fetchExchange],
+})
+
+const getPokemons = gql(`
+  query getPokemons {
+    pokemon_v2_pokemon(limit:30, where:{pokemon_species_id: {_gt: 500}}) {
+      name
+      id
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+    }
+  }
+`)
+
+const pokemonQuery = selector({
+  key: "date-query/recoil-pokemon",
+  get: async ({}) => {
+    const result = await client.query(getPokemons, {}).toPromise()
+    return result
+  },
+})
+
+const useRecoilUrql = async () => {
+  const date = useRecoilValue(pokemonQuery)
+  console.log({ date })
+  return date
+}
+
 export const RecoilUrql = () => {
+  const date = useRecoilUrql()
   return <>Recoil</>
 }
